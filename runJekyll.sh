9e1a6f6ce5a2a934031ff1bf7f17ea304afcc58e
Volker Benders <vbenders@gmail.com>
2024-01-30 00:47:01 +0100

    wip

--- runJekyll.sh
+++ runJekyll.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+image="jekyll/jekyll:3.8"
+image="richardsoper/jekyll:latest"
+image="starefossen/github-pages"
+#echo \
+#docker run \
+#    --volume="${PWD}:/srv/jekyll" \
+#    -p 4000:4000 \
+#    -it $image \
+#    jekyll serve --incremental --drafts
 
 docker run \
-    --volume="${PWD}:/srv/jekyll" \
-    -p 4000:4000 \
-    -it jekyll/jekyll:3.8 \
-    jekyll serve --incremental --drafts
-
+    -it \
+    --rm \
+    -v "$PWD":/usr/src/app \
+    -p "4000:4000" \
+    $image #\
+    #serve --incremental --drafts #starefossen/github-pages 
